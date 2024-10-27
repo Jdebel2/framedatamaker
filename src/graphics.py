b80bdb2a181373d57900a8644a6cbc4fc7f5524e
jdebel2 <jdebel2@gmail.com>
2024-10-27 00:41:03 -0400

    feat/addbutton added button class and new button

--- src/graphics.py
+++ src/graphics.py
@@ -1,4 +1,11 @@
-from tkinter import Tk, Canvas, BOTH, PhotoImage, Label, NW
+from tkinter import Tk, Canvas, BOTH, PhotoImage, Label, NW, Button
+from enum import Enum
+
+
+class ButtonFunction(Enum):
+    NEW=1
+    LOAD=2
+
 
 class Window():
     def __init__(self, width, height):
@@ -10,8 +17,16 @@ class Window():
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
 
+    def get_root(self):
+        return self.__root
+
+
     def draw_image(self, image, x, y):
         self.__canvas.create_image(x,y,anchor=NW,image=image)
+
+
+    def draw_button(self, btn, x, y):
+        btn.place(x=x, y=y)
 
 
     def redraw(self):
@@ -30,7 +45,7 @@ class Window():
         self.__running = False
 
 
-class Sprite():
+class FDMImage():
     def __init__(self, spr_path, x, y, win):
         self.img = PhotoImage(file=spr_path)
         self.x = x
@@ -42,3 +57,23 @@ class Sprite():
         self.win.draw_image(self.img, self.x, self.y)
         
 
+class FDMButton():
+    def __init__(self,text, x, y, width, height, mode, win):
+        command=None
+        match (mode): 
+            case ButtonFunction.NEW:
+                command=self.create_new
+            case _:
+                raise NotImplementedError("Only NEW is implemented")
+        self.btn = Button(win.get_root(), text=text, width=width, height=height, bd='1', command=command)
+        self.x = x
+        self.y = y
+        self.win=win
+    
+
+    def draw(self):
+        self.win.draw_button(self.btn, self.x, self.y)
+    
+
+    def create_new(self):
+        print("We are making frame data!")
--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
-from graphics import Window, Sprite
+from graphics import Window, FDMImage, FDMButton, ButtonFunction
 from tkinter import *
 
 def main():
     win = Window(800, 600)
-    spr = Sprite('assets/test_sprite.png', 100, 100, win)
+    spr = FDMImage('assets/test_sprite.png', 100, 100, win)
     spr.draw()
+    btn = FDMButton('New', 20, 20, 6, 1, ButtonFunction.NEW, win)
+    btn.draw()
     win.wait_for_close()
 
 if __name__ == '__main__':
